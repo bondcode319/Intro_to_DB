d10457acc649717ed9260e6fe3325131fb839aa9
brianbond <tumuhimbise92@gmail.com>
2025-06-28 13:45:29 +0300

    second 54

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -27,7 +27,7 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE Order_details (
+CREATE TABLE Order_Details (
     orderdetailid INT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
