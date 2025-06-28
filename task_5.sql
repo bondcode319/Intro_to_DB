bbb69326eb6de12f532e137dd8e7cd7687a2caee
brianbond <tumuhimbise92@gmail.com>
2025-06-28 14:23:14 +0300

    second 04

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,5 @@
-INSERT INTO Employees (EmployeeID, FirstName, LastName, Email, HireDate)
-VALUES (1, 'John', 'Doe', 'john.doe@example.com', '2022-05-15');
 
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+
+INSERT INTO Customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave');
